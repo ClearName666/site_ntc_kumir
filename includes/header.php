72d5ce61e0aa9fe28a97c20a4f4feed584054433
pavel <pavelstorozhev10@gmail.com>
2026-06-15 13:51:51 +0800

    header

--- includes/header.php
+++ includes/header.php
@@ -45,13 +45,17 @@ $isAdmin = isset($_SESSION['admin_id']);
                 <div class="header-right">
                     <!-- Телефон -->
                     <a href="tel:<?php echo preg_replace('/[^0-9+]/', '', getSetting($conn, 'phone')); ?>" class="header-phone">
-                        <span class="phone-icon">📞</span>
+                        <span class="phone-icon">
+                            <img src="/assets/images/static/phone-icon.png" alt="Телефон" class="icon-img">
+                        </span>
                         <span><?php echo getSetting($conn, 'phone'); ?></span>
                     </a>
                     
                     <!-- Личный кабинет  заменить на /userKumir/index.php-->
                     <a href="https://v4.ntckumir.ru/" class="btn-personal">
-                        <span class="btn-icon">👤</span>
+                        <span class="btn-icon">
+                            <img src="/assets/images/static/user-icon.png" alt="Личный кабинет" class="icon-img">
+                        </span>
                         <span class="btn-text">Личный кабинет</span>
                     </a>
                     
@@ -81,12 +85,16 @@ $isAdmin = isset($_SESSION['admin_id']);
         
         <div class="mobile-contact">
             <a href="tel:<?php echo preg_replace('/[^0-9+]/', '', getSetting($conn, 'phone')); ?>" class="header-phone">
-                <span class="phone-icon">📞</span>
+                <span class="phone-icon">
+                    <img src="/assets/images/static/phone-icon.png" alt="Телефон" class="icon-img">
+                </span>
                 <span><?php echo getSetting($conn, 'phone'); ?></span>
             </a>
             
             <a href="https://v4.ntckumir.ru/" class="btn-personal">
-                <span class="btn-icon">👤</span>
+                <span class="btn-icon">
+                    <img src="/assets/images/static/user-icon.png" alt="Личный кабинет" class="icon-img">
+                </span>
                 <span class="btn-text">Личный кабинет</span>
             </a>
             
